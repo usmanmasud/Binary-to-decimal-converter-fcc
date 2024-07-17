42ed49e92ada1dbb21d52915f2a7d0c5f6403a90
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-17 21:40:14 +0100

    script updated

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+
+  }
+
+  console.log(numberInput.value);
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
