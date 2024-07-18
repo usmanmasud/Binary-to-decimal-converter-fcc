8e2f213f35894ff96411b65109a3492b43b1d9f3
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-18 14:57:06 +0100

    binary convertion function refactoring

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
+
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
+  let binary = "";
 
   if (input === 0) {
-    result.innerText = "0";
-    return;
+    binary = "0";
   }
 
   while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
+    binary = (input % 2) + binary;
+    input = Math.floor(input / 2);
   }
 
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
-
-  result.innerText = remainders.reverse().join("");
+  result.innerText = binary;
 };
 
 const checkUserInput = () => {
