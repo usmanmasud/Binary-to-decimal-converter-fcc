292009dcfe68416f616bcaff771cb7ebfef52a62
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-18 14:42:40 +0100

    binary  convertion functionality added

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
+
+  if (input === 0) {
+    result.innerText = "0";
+    return;
+  }
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
+};
+
 const checkUserInput = () => {
   if (
     !numberInput.value ||
@@ -9,10 +36,11 @@ const checkUserInput = () => {
     parseInt(numberInput.value) < 0
   ) {
     alert("Please provide a decimal number greater than or equal to 0");
-
+    return;
   }
 
-  console.log(numberInput.value);
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
